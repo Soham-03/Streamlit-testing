806188c32c760e17f078e56feed789c352b1f9f7
Soham Parab <sohamparab1003@gmail.com>
2024-06-14 21:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,42 +19,46 @@ def get_pdf_text(pdf_docs):
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text() or ""  # Ensuring that None doesn't break concatenation
+            text += page.extract_text()
     return text
 
-def get_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    return text_splitter.split_text(text)
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000,
+        chunk_overlap=1000
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
-def get_conversational_chain():
+def get_conversation_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    the provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n{context}\n
-    Question:\n{question}\n
-
-    Answer:
+        Answer the question clear and precise. If not provided the context return the result as
+        "Sorry I dont know the answer", don't provide the wrong answer.
+        Context:\n {context}?\n
+        Question:\n{question}\n
+        Answer:
     """
-    
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    
-    return load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
+    chain = load_qa_chain(model, chain_type='stuff', prompt=prompt)
+    return chain
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
 
-    chain = get_conversational_chain()
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-
-    st.write("Reply: ", response["output_text"])
+    chain = get_conversation_chain()
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+    st.write(user_template.replace("{{MSG}}", response["output_text"]), unsafe_allow_html=True)
 
 def main():
     st.set_page_config(page_title="Chat PDF", layout="wide")
